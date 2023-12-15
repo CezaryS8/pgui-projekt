1f3afefe85a4428fafd06ffc676ff5005014fd48
Szade <wojciech.szade@gmail.com>
2023-12-15 15:00:23 +0100

    Updated navbar (more components)

---
+++ components/Navbar/UserDetails.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+
+const UserDetails = () => {
+    return (
+        <div>
+            <p>Imię Nazwisko</p>
+            <p>Zdjęcie profilowe</p>
+            <p>🔽</p>
+        </div>
+    )
+}
+
+export default UserDetails;
