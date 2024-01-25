1f73ef9dec4ebde51df59c42e50798260a41051a
Wojciech Szade <wojciech.szade@gmail.com>
2024-01-25 14:26:28 +0100

    Created common CategorySelector

---
+++ components/commons/CategorySelector.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const CategorySelector = ({ isSelected, children }) => {
+  const categoryStyle = {
+    fontSize: "0.875rem",
+    color: isSelected ? "#6200ea" : "rgba(0, 0, 0, 0.54)",
+    fontWeight: isSelected ? 600 : 400,
+    cursor: "pointer",
+    "&:hover": {
+      color: "#6200ea",
+    },
+  };
+
+  return (
+    <Typography variant="body2" style={categoryStyle}>
+      {children}
+    </Typography>
+  );
+};
+
+export default CategorySelector;
