e31139ec1d753d1f7b5be6cf966e3362153e78c1
Wojciech Szade <wojciech.szade@gmail.com>
2024-01-25 14:27:46 +0100

    Created test page for development purpose

---
+++ src/app/test/page.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid, Paper } from "@mui/material";
+
+import OrdersWidget from "../../../components/OrdersWidget/OrdersWidget";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+import Navbar from "../../../components/Navbar/Navbar";
+import QualityWidget from "../../../components/QualityWidget/QualityWidget";
+import ReviewsWidget from "../../../components/ReviewsWidget/ReviewsWidget";
+// import SalesChartWidget from "./../../components/SalesChartWidget/SalesChartWidget";
+// import QualityWidget from "./../../components/QualityWidget/QualityWidget";
+// import RankingWidget from "./../../components/RankingWidget/RankingWidget";
+// import ReviewsWidget from "./../../components/ReviewsWidget/ReviewsWidget";
+// import SalesTipsWidget from "./../../components/SalesTipsWidget/SalesTipsWidget";
+// import OrdersWidgetChat from "./../../components/OrdersWidgetChat";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <ReviewsWidget />
+    </div>
+  );
+};
+
+export default Dashboard;
