d5f031b30699b34c97ac8ba92a4021eaf81e17a6
cezarys8 <czarek.skorupski2001@gmail.com>
2024-01-26 01:54:32 +0100

    manual prettier forntend

--- components/ReviewsWidget/Review.js
+++ components/ReviewsWidget/Review.js
@@ -3,33 +3,28 @@ import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import DateDisplay from "./../commons/DateDisplay";
 
-
 const Review = ({ product, review, date }) => {
-    const textStyle = {
-        fontFamily: "Poppins",
-        fontWeight: 600,
-        lineHeight: "22px",
-        letterSpacing: "0px",
-        textAlign: "left",
-    };
+  const textStyle = {
+    fontFamily: "Poppins",
+    fontSize: "14px",
+    fontWeight: 400,
+    lineHeight: "22px",
+    letterSpacing: "0px",
+    textAlign: "left",
+  };
 
-    return (
-        <div style={{ display: "flex", alignItems: "center" }}>
-            <DateDisplay date={date} />
-            <Rating
-                name="read-only"
-                value={review}
-                readOnly
-                sx={{ ...textStyle, marginLeft: 2 }}
-            />
-            <Typography
-                variant="body2"
-                sx={{ ...textStyle, fontWeight: "medium", marginLeft: 2 }}
-            >
-                {product}
-            </Typography>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <DateDisplay date={date} sx={{ ...textStyle }} />
+      <Rating
+        name="read-only"
+        value={review}
+        readOnly
+        sx={{ ...textStyle, marginLeft: 2 }}
+      />
+      <Typography sx={{ ...textStyle, marginLeft: 2 }}>{product}</Typography>
+    </div>
+  );
 };
 
 export default Review;
